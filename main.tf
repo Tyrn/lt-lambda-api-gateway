58ce866f209dfb66eb1180fbb3a615d1986b84d1
Tyrn <a@b.com>
2021-11-03 17:38:45 +0300

    To Python

--- main.tf
+++ main.tf
@@ -55,8 +55,8 @@ resource "aws_lambda_function" "hello_world" {
   s3_bucket = aws_s3_bucket.lambda_bucket.id
   s3_key    = aws_s3_bucket_object.lambda_hello_world.key
 
-  runtime = "nodejs12.x"
-  handler = "hello.handler"
+  runtime = "python3.8"
+  handler = "main.lambda_handler"
 
   source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
 
